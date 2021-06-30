d067a29c18e0806c99b6bdba1fff598b1d86b276
dima92 <dima-tkachenko@mail.ru>
2021-07-01 02:45:16 +0300

    feat: ended Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newConfig = []
+    bracketsConfig.forEach((el) => {
+        newConfig.push(el.join(''))
+    })
+
+    let search = 1, l
+
+    while (search) {
+        search = 0
+        newConfig.forEach((el) => {
+            l = str.length
+            str = str.replace(el, '')
+            if (l > str.length) search++
+        })
+    }
+
+    return str.length === 0
 }
